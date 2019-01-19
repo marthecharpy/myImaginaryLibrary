f6a12fb3e262fb8d2b882cb83e000ad8da94b2a3
marthe.charpy <marthe.charpy@gmail.com>
2019-01-19 15:02:05 +0100

    first commit

---
+++ src/main/java/com/wildcodeschool/myImaginaryLibrary/MyImaginaryLibraryApplication.java
@@ -0,0 +1,14 @@
+package com.wildcodeschool.myImaginaryLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyImaginaryLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyImaginaryLibraryApplication.class, args);
+	}
+
+}
+
